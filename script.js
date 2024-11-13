6c4365a1a788b7d93a86bf7d860d17f02c937386
Satyaki Biswas <106860871+SatyakiSpirit@users.noreply.github.com>
2024-11-13 09:31:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const lastChar = display.value[display.value.length - 1];
+    if (lastChar && '+-*/'.includes(lastChar)) {
+        display.value = display.value.slice(0, -1) + operator;
+    } else {
+        display.value += operator;
+    }
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
